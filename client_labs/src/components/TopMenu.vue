2783e3534d68abeafa45a0ac42c922498e16709a
Amitav Roy <reachme@amitavroy.com>
2016-10-18 16:38:31 +0530

    Removed the form from top menu

--- client_labs/src/components/TopMenu.vue
+++ client_labs/src/components/TopMenu.vue
@@ -41,14 +41,7 @@
             </ul>
           </li>
         </ul>
-        <form class="navbar-form navbar-left">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Link</a></li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
             <ul class="dropdown-menu">
